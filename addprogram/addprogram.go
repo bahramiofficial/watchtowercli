2fec42e640fcaf9aedb239813b1df8cfd552572c
peyman bahrami <77381943+bahramiofficial@users.noreply.github.com>
2025-01-01 10:38:56 +0330

    add code for get dynamic path json

--- addprogram/addprogram.go
+++ addprogram/addprogram.go
@@ -1,7 +1,21 @@
 package addprogram
 
-import watchsyncprograms "github.com/bahramiofficial/watchtower/src/utilities/programs"
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+
+	watchsyncprograms "github.com/bahramiofficial/watchtower/src/utilities/programs"
+)
 
 func AddPrograms() {
-	watchsyncprograms.SyncProgramToDb()
+	// Get the directory of the script
+	_, scriptPath, _, ok := runtime.Caller(0)
+	if !ok {
+		log.Fatalf("Failed to get the script directory")
+	}
+
+	// init path json files
+	dirPath := filepath.Join(filepath.Dir(scriptPath), "json")
+	watchsyncprograms.SyncProgramToDb(dirPath)
 }
